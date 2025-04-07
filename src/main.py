ec7f2412014cabbcba916b2325f849c616c3e16f
D3c3453d <sergey.et02@gmail.com>
2025-04-07 23:18:00 +0300

    feat: added models

--- src/main.py
+++ src/main.py
@@ -1,8 +1,17 @@
 import os
 
 import cv2
-import methods
+import numpy as np
 import pandas as pd
+from catboost import CatBoostClassifier
+from methods import brenner_gradient, laplacian, sobel_variance, tenengrad
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import precision_score, recall_score
+from sklearn.model_selection import train_test_split
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.tree import DecisionTreeClassifier
+
+FEAT_COLUMNS = [f.__name__ for f in [brenner_gradient, laplacian, sobel_variance, tenengrad]]
 
 
 def extract_frames(video_path: str, output_folder: str, frame_interval: int = 1):
@@ -11,9 +20,9 @@ def extract_frames(video_path: str, output_folder: str, frame_interval: int = 1)
     frame_count = 0
     saved_count = 0
     if cap.isOpened():
-        print("Video file successfully retrieved")
+        print("Video file successfully retrieved\n")
     else:
-        print("Video file wasn't retrieved properly")
+        print("Video file wasn't retrieved properly\n")
     while True:
         ret, frame = cap.read()
         if not ret:
@@ -23,33 +32,87 @@ def extract_frames(video_path: str, output_folder: str, frame_interval: int = 1)
             saved_count += 1
         frame_count += 1
     cap.release()
-    print(f"Extracted {saved_count}/{frame_count} frames to {output_folder}")
+    print(f"Extracted {saved_count}/{frame_count} frames to {output_folder}\n")
 
 
-def create_dataframe(image_folder):
+def compute_features(image: np.ndarray):
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)  # Convert to grayscale
+    return [
+        brenner_gradient(gray),
+        sobel_variance(gray),
+        tenengrad(gray),
+        laplacian(gray),
+    ]
+
+
+def create_dataframe(image_folder: str):
     features = []
     files = [f for f in os.listdir(image_folder) if f.endswith(".jpg")]
     files.sort()
     for file in files:
         image = cv2.imread(f"{image_folder}/{file}")
-        features.append(
-            [
-                file,
-                methods.compute_brenner_gradient(image),
-                methods.compute_sobel_variance(image),
-                methods.compute_tenengrad(image),
-                methods.compute_laplacian(image),
-            ]
+        features.append([file] + compute_features(image))
+    return pd.DataFrame(features, columns=["file"] + FEAT_COLUMNS)
+
+
+def label_dataframe(df: pd.DataFrame, image_folder: str):
+    files = [f for f in os.listdir(image_folder) if f.endswith(".jpg")]
+    df["label"] = (df["file"].isin(files)).astype(int)
+    return df
+
+
+def train_models(X_train, y_train):
+    models = {
+        "Logistic Regression": LogisticRegression(),
+        "KNN": KNeighborsClassifier(),
+        "Decision Tree": DecisionTreeClassifier(),
+        "CatBoost": CatBoostClassifier(verbose=0),
+    }
+    for name, model in models.items():
+        model.fit(X_train, y_train)
+    return models
+
+
+def evaluate(models: dict, X_test, y_test):
+    results = []
+    for name, model in models.items():
+        y_pred = model.predict(X_test)
+        results.append(
+            {"Model": name, "Precision": precision_score(y_test, y_pred), "Recall": recall_score(y_test, y_pred)}
         )
-    return pd.DataFrame(features, columns=["file", "brenner", "sobel", "tenengrad", "laplacian"])
+    return pd.DataFrame(results)
+
+
+def predict_image(image_path: str, models: dict):
+    results = []
+    image = cv2.imread(image_path)
+    features = pd.DataFrame([compute_features(image)], columns=FEAT_COLUMNS)
+    for name, model in models.items():
+        results.append({"Model": name, "Prediction": model.predict(features)[0]})
+    return pd.DataFrame(results)
 
 
 if __name__ == "__main__":
-    video_path = "autofocus1.mp4"
-    image_folder = "./frames/all/"
-    result_path = "result.json"
+    video_path = "./autofocus1.mp4"
+    all_folder = "./frames/all/"
+    focus_folder = "./frames/focus/"
     frame_interval = 7
 
-    extract_frames(video_path, image_folder, frame_interval)
-    df = create_dataframe(image_folder)
-    print(df.to_string())
+    # Prepare
+    extract_frames(video_path, all_folder, frame_interval)
+    df = create_dataframe(all_folder)
+    df = label_dataframe(df, focus_folder)
+    print(f"Data\n{df.to_string()}\n")
+
+    X = df[FEAT_COLUMNS]
+    y = df["label"]
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
+
+    # Train-evaluate
+    models = train_models(X_train, y_train)
+    results = evaluate(models, X_test, y_test)
+    print(f"Train results\n{results}\n")
+
+    # Predict
+    results = predict_image("./maxim-bogdanov-wjAR4jo979Y-unsplash.jpg", models)
+    print(f"Prediction\n{results}\n")
--- src/methods.py
+++ src/methods.py
@@ -2,31 +2,27 @@ import cv2
 import numpy as np
 
 
-def compute_brenner_gradient(image):
-    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)  # Convert to grayscale
-    shifted = np.roll(gray, -2, axis=1)  # Shift by 2 pixels horizontally
-    diff = (gray - shifted) ** 2  # Compute squared difference
+def brenner_gradient(gray_image: np.ndarray):
+    shifted = np.roll(gray_image, -2, axis=1)  # Shift by 2 pixels horizontally
+    diff = (gray_image - shifted) ** 2  # Compute squared difference
     return np.sum(diff)  # Sum all differences as the focus measure
 
 
-def compute_sobel_variance(image):
-    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)  # Convert to grayscale
-    sobel_x = cv2.Sobel(gray, cv2.CV_64F, 1, 0, ksize=3)  # Sobel X gradient
-    sobel_y = cv2.Sobel(gray, cv2.CV_64F, 0, 1, ksize=3)  # Sobel Y gradient
+def sobel_variance(gray_image: np.ndarray):
+    sobel_x = cv2.Sobel(gray_image, cv2.CV_64F, 1, 0, ksize=3)  # Sobel X gradient
+    sobel_y = cv2.Sobel(gray_image, cv2.CV_64F, 0, 1, ksize=3)  # Sobel Y gradient
     sobel_magnitude = np.sqrt(sobel_x**2 + sobel_y**2)  # Compute gradient magnitude
-    variance = np.var(gray)  # Compute variance of pixel intensities
+    variance = np.var(gray_image)  # Compute variance of pixel intensities
     return np.mean(sobel_magnitude) + variance  # Combine Sobel and variance
 
 
-def compute_tenengrad(image):
-    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)  # Convert to grayscale
-    sobel_x = cv2.Sobel(gray, cv2.CV_64F, 1, 0, ksize=3)  # Sobel filter in X direction
-    sobel_y = cv2.Sobel(gray, cv2.CV_64F, 0, 1, ksize=3)  # Sobel filter in Y direction
+def tenengrad(gray_image: np.ndarray):
+    sobel_x = cv2.Sobel(gray_image, cv2.CV_64F, 1, 0, ksize=3)  # Sobel filter in X direction
+    sobel_y = cv2.Sobel(gray_image, cv2.CV_64F, 0, 1, ksize=3)  # Sobel filter in Y direction
     tenengrad = np.sqrt(sobel_x**2 + sobel_y**2)  # Compute gradient magnitude
     return np.mean(tenengrad)  # Return mean gradient magnitude as focus score
 
 
-def compute_laplacian(image):
-    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)  # Convert to grayscale
-    laplacian = cv2.Laplacian(gray, cv2.CV_64F)  # Apply Laplacian filter
+def laplacian(gray_image: np.ndarray):
+    laplacian = cv2.Laplacian(gray_image, cv2.CV_64F)  # Apply Laplacian filter
     return np.var(laplacian)  # Compute variance of Laplacian
