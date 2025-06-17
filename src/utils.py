db376d5eb1e75cac0d23cce958008dc623837a0d
D3c3453d <sergey.et02@gmail.com>
2025-06-17 14:13:45 +0300

    feat: resolution test

--- src/utils.py
+++ src/utils.py
@@ -1,9 +1,11 @@
 import os
+import time
 
 import cv2
 import pandas as pd
 from features import FeatureExtractor
 from features0 import FeatureExtractor0
+from settings import PREDICT_DIR
 
 
 def extract_frames(video_path: str, output_folder: str, frame_interval: int = 1):
@@ -29,12 +31,29 @@ def extract_frames(video_path: str, output_folder: str, frame_interval: int = 1)
 
 feat_ext = FeatureExtractor()
 feat_ext_0 = FeatureExtractor0()
-img_path = "./data/good/0_IPHONE-SE_S.JPG"
+img_path = "./data/predict/0050.jpg"
 
-data = [
-    {"name": "current", **feat_ext.extract_features_path(img_path)},
-    {"name": "original", **feat_ext_0.extract_features_path(img_path)},
-]
+# data = [
+#     {"name": "current", **feat_ext.extract_features_path(img_path)},
+#     {"name": "original", **feat_ext_0.extract_features_path(img_path)},
+# ]
 
+data = []
+resize_list = [(3840, 2160), (2560, 1440), (1920, 1080), (1280, 720), (640, 360)]
+for file in list(PREDICT_DIR.glob("*"))[:10]:
+    print(file)
+    orig_img = cv2.imread(str(file))
+    for w, h in resize_list:
+        temp = {}
+        img = cv2.resize(orig_img, (w, h))
+        temp["res"] = f"{w}x{h}"
+        start = time.time()
+        feat_ext_0.extract_features_path(img_path)
+        temp["old"] = time.time() - start
+        start = time.time()
+        feat_ext.extract_features_path(img_path)
+        temp["new"] = time.time() - start
+        data.append(temp)
+        print(f"{w}x{h} done")
 df = pd.DataFrame(data)
-print(df)
+print(df.sort_values("res"))
